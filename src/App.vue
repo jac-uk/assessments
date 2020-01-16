01a0d35f168460b705e7a6ae0b24c5933953c646
Martin R <martin.raymundo@judicialappointments.digital>
2020-01-16 13:14:36 +0100

    fixing file upload

--- src/App.vue
+++ src/App.vue
@@ -39,20 +39,141 @@
     </div>
 
     <main id="main-content">
-      <Upload />
+      <div>
+        <form @submit.prevent="save">
+          <div class="govuk-grid-column-two-thirds">
+            <h1 class="govuk-heading-l">
+              Upload Independent Assessments
+            </h1>
+
+            <div class="govuk-form-group">
+              <label
+                class="govuk-heading-m"
+                for="independent-assessment-file"
+              >
+                Please upload your assessment here
+              </label>
+              <input
+                id="independent-assessment-file"
+                class="govuk-file-upload"
+                type="file"
+                @change="fileSelected"
+              >
+            </div>
+
+            <button class="govuk-button">
+              Upload File
+            </button>
+          </div>
+        </form>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
-import Upload from './components/Upload.vue';
+import firebase from '@firebase/app';
+import '@firebase/storage';
 
 export default {
-  name: 'app',
-  components: {
-    Upload
-  }  
-}
+  name: 'App',
+  components: {},
+  computed: {
+    applicationId() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const myParam = urlParams.get('reference-num');
+      return myParam;
+    },
+    assessorEmail() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const myParam = urlParams.get('assessor');
+      return myParam;        
+    },
+  },
+  methods: {
+    //async save(isValid) {
+    async save() {
+      await this.upload();
+      alert('Thanks for uploading your assessment');
+    },
+    fileSelected(event) {
+      //console.log('fileSelected called');
+      if (event.target.files.length > 0) {
+        //console.log(event.target.files.length);
+        const file = event.target.files[0];
+        //console.log(file);
+
+        const data = {
+          name: file.name,
+          file: file,
+          type: event.target.id,
+        };
+        console.log(data);
+      }
+    },
+    upload() {
+      console.log('upload function called');
+      //console.log(`item.name = ${item.name}`);
+      //console.log(`item.type = ${item.type}`);
+      //console.log('item = ', item);
+
+      const file = document.getElementById('independent-assessment-file').files[0];
+      const fileExtension = file.name.split('.')[1];
+      //console.log(fileExtension);
+
+      const urlParams = new URLSearchParams(window.location.search);
+      const assessorEmail = urlParams.get('assessor');
+      const applicationId = urlParams.get('applicationId');
+
+      const fileSavePath = `application-${applicationId}/${assessorEmail}-independent-assessment.${fileExtension}`;
+      console.log(`fileSavePath = ${fileSavePath}`);
+
+      const storageRef = firebase.storage().ref();
+
+      let uploadTask = storageRef.child(fileSavePath).put(file);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask
+        .on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+            (snapshot) => {
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              //let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              //console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+              case firebase.storage.TaskState.PAUSED: // or 'paused'
+                //console.log('Upload is paused');
+                break;
+              case firebase.storage.TaskState.RUNNING: // or 'running'
+                //console.log('Upload is running');
+                break;
+              }
+            }, (error) => {
+
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              switch (error.code) {
+              case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
+
+              case 'storage/canceled':
+                // User canceled the upload
+                break;
+
+              case 'storage/unknown':
+                // Unknown error occurred, inspect error.serverResponse
+                break;
+              }
+            }, () => {
+              // Upload completed successfully, now we can get the download URL
+              uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                //console.log('File available at', downloadURL);
+                alert(`downloadURL = ${downloadURL}`);
+              });
+            });
+    },
+  },     
+};
 </script>
 
 <style lang="scss">
