8dfe1fcb5eda7d3517f9936bdc8e4b26b4ea1eec
Lisias (Lee) Loback <lisias@loback.co.uk>
2020-11-17 22:35:43 +0000

    #95 Handle assessment being missing/cancelled/deleted - Lint fixing

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -9,8 +9,20 @@
         <h1 class="govuk-heading-l">
           Assessments
         </h1>
-        <div v-if="loginFail" aria-labelledby="error-summary-title" role="alert" tabindex="-1" data-module="govuk-error-summary" class="govuk-error-summary">
-          <h2 id="error-summary-title" class="govuk-error-summary__title"> There is a problem </h2>
+        <div
+          v-if="loginFail"
+          aria-labelledby="error-summary-title"
+          role="alert"
+          tabindex="-1"
+          data-module="govuk-error-summary"
+          class="govuk-error-summary"
+        >
+          <h2
+            id="error-summary-title"
+            class="govuk-error-summary__title"
+          >
+            There is a problem
+          </h2>
           <div class="govuk-error-summary__body">
             <ul class="govuk-list govuk-error-summary__list">
               <li>You may have the wrong link, or</li>
