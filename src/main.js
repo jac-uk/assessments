5be72c36a5f3ec423b4ea2508233b39a5262a318
halcyonjac <halcyon@judicialappointments.digital>
2022-09-27 14:10:25 +0100

    add sentry/vue and sentry/tracing

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ import * as filters from '@/filters';
 import router from '@/router';
 import store from '@/store';
 import { auth } from '@/firebase';
-import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
+import * as Sentry from '@sentry/vue';
+import { BrowserTracing } from '@sentry/tracing';
 
 if (process.env.NODE_ENV !== 'development') {
   Sentry.init({
+    Vue,
     dsn: 'https://23ac92825117451eb421535be7e4c334@o323827.ingest.sentry.io/5301649',
     environment: store.getters.appEnvironment.toLowerCase(),
     release: process.env.PACKAGE_VERSION,
-    integrations: [new Integrations.Vue({ Vue, attachProps: true })],
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      }),
+    ],
   });
 }
 Vue.config.productionTip = false;
