4ceb162bc950a779939619110499b3f357e6582e
drieJAC <jac.drie@digitalteam.uk>
2024-11-20 18:01:56 +0000

    Revert "Debugging issue with sign in not working (#199)"
    This reverts commit 10c3062b1f40d1c3b049004ffa882c18d317f5b6.

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -57,71 +57,42 @@ export default {
     };
   },
   async created() {
-
-    try {
-
-      console.log('this.$route.query:');
-      console.log(this.$route.query);
-
-      if (this.$route.query.email && this.$route.query.ref) {
-
-        console.log('A');
-
-        // we have email and ref querystring parameters so try to sign in automatically
-        const email = this.$route.query.email.replace(/ /g, '+');  // Quick fix for #28 `+` being stripped from emails in IA links
-        const ref = this.$route.query.ref;
-
-        console.log(`email: ${email}`);
-        console.log(`ref: ${ref}`);
-
-        const returnUrl = `${window.location.protocol}//${window.location.host}/sign-in`;
-        const response = await httpsCallable(functions, 'generateSignInWithEmailLink')({ ref: ref, email: email, returnUrl: returnUrl });
-
-        console.log('RESPONSE:');
-        console.log(response);
-
-        if (response && response.data && response.data.result) {
-
-          console.log('response.data.result:');
-          console.log(response.data.result);
-
-          window.localStorage.setItem('emailForSignIn', email);
-          window.localStorage.setItem('signInDestination', `${ref}/upload`);
-          return window.location.replace(response.data.result);
-        } else {
-          console.log('mal-formed request');
-          this.loginFail = true;
-          this.signOut();
-        }
-      } else if (this.$route.query.return) {
-
-        console.log('B');
-
-        // we have 'return' flag set so try to complete sign in
-        if (isSignInWithEmailLink(auth, window.location.href)) {
-          const email = window.localStorage.getItem('emailForSignIn');
-          const ref = window.localStorage.getItem('signInDestination');
-          if (email && ref) {
-            const result = await signInWithEmailLink(auth, email, window.location.href);
-            // email in user object from signInWithEmailLink will be converted to lowercase which might cause query issue
-            if (result && result.user && result.user.email !== email) {
-              result.user.email = email;
-            }
-            window.localStorage.removeItem('emailForSignIn');
-            window.localStorage.removeItem('signInDestination');
-            await this.$store.dispatch('auth/setCurrentUser', result.user);
-            this.$router.replace(ref);
-          } else {
-            this.loginFail = true;
+    if (this.$route.query.email && this.$route.query.ref) {
+      // we have email and ref querystring parameters so try to sign in automatically
+      const email = this.$route.query.email.replace(/ /g, '+');  // Quick fix for #28 `+` being stripped from emails in IA links
+      const ref = this.$route.query.ref;
+      const returnUrl = `${window.location.protocol}//${window.location.host}/sign-in`;
+      const response = await httpsCallable(functions, 'generateSignInWithEmailLink')({ ref: ref, email: email, returnUrl: returnUrl });
+      if (response && response.data && response.data.result) {
+        window.localStorage.setItem('emailForSignIn', email);
+        window.localStorage.setItem('signInDestination', `${ref}/upload`);
+        return window.location.replace(response.data.result);
+      } else {
+        // console.log('mal-formed request');
+        this.loginFail = true;
+        this.signOut();
+      }
+    } else if (this.$route.query.return) {
+      // we have 'return' flag set so try to complete sign in
+      if (isSignInWithEmailLink(auth, window.location.href)) {
+        const email = window.localStorage.getItem('emailForSignIn');
+        const ref = window.localStorage.getItem('signInDestination');
+        if (email && ref) {
+          const result = await signInWithEmailLink(auth, email, window.location.href);
+          // email in user object from signInWithEmailLink will be converted to lowercase which might cause query issue
+          if (result && result.user && result.user.email !== email) {
+            result.user.email = email;
           }
+          window.localStorage.removeItem('emailForSignIn');
+          window.localStorage.removeItem('signInDestination');
+          await this.$store.dispatch('auth/setCurrentUser', result.user);
+          this.$router.replace(ref);
+        } else {
+          this.loginFail = true;
         }
       }
-      this.loading = false;
     }
-    catch (err) {
-      console.log('ERR:');
-      console.log(err);
-    }
+    this.loading = false;
   },
   methods: {
     async signOut() {
